d4a5f3240981b2cc5de0c4ebc89daa69dbd73f22
xiker <xikerromero@gmail.com>
2014-05-27 16:57:20 +0200

    hola, esto es un commit
    Esto es un commit, y esto es información adicional sobre el commit por
    poner texto descriptivo aleatorio

---
+++ PruebaA/src/pruebaa/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaa;
+
+/**
+ *
+ * @author xiker
+ */
+public class Persona {
+   
+    public void Saludar(){
+    System.out.println("Hey man!!!");
+    }
+}
---
+++ PruebaA/src/pruebaa/PruebaA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaa;
+
+/**
+ *
+ * @author xiker
+ */
+public class PruebaA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
